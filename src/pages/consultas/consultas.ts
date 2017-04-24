7d5acabb0e195a9134323298d7be3d201da42704
Juan Mora <raicerk@gmail.com>
2017-04-24 01:04:28 -0300

    Inicio del proyecto con IONIC y angular4, desarrollando una app para android

---
+++ src/pages/consultas/consultas.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'Consultas',
+  templateUrl: 'consultas.html'
+})
+export class Consultas {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
